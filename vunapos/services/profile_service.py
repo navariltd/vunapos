db4460a7bdb52ca9f7f66ffcac734ddb78359f89
Brian Mwambia <mwambia@navari.co.ke>
2026-07-09 18:06:57 +0300

    fix(pos): improve bootstrap profile resolution and session validation                                                      - Validate POS Profile assignment before loading POS - Add POS Opening Entry status to bootstrap response - Return POS session readiness state for React client - Prevent users without enabled POS Profiles from accessing POS - Prepare bootstrap flow for opening entry requirement - Improve POS initialization response structure

--- vunapos/services/profile_service.py
+++ vunapos/services/profile_service.py
@@ -15,25 +15,65 @@ def get_invoice_mode():
 
 
 def resolve_pos_profile(pos_profile=None):
-	if pos_profile:
-		require_read("POS Profile", pos_profile)
-		profile = frappe.get_doc("POS Profile", pos_profile)
-	else:
-		filters = {"disabled": 0}
-		user_profile = frappe.db.get_value("POS Profile User", {"user": frappe.session.user}, "parent")
-		profile_name = user_profile or frappe.db.get_value(
-			"POS Profile", filters, "name", order_by="modified desc"
-		)
-		if not profile_name:
-			frappe.throw(_("No enabled POS Profile found"))
-		require_read("POS Profile", profile_name)
-		profile = frappe.get_doc("POS Profile", profile_name)
 
-	if profile.get("disabled"):
-		frappe.throw(_("POS Profile {0} is disabled").format(profile.name))
+    if pos_profile:
 
-	return profile
+        require_read(
+            "POS Profile",
+            pos_profile
+        )
 
+        profile = frappe.get_cached_doc(
+            "POS Profile",
+            pos_profile
+        )
+
+    else:
+
+        profiles = frappe.get_all(
+            "POS Profile User",
+            filters={
+                "user": frappe.session.user
+            },
+            pluck="parent"
+        )
+
+        if not profiles:
+            frappe.throw(
+                _("No POS Profile assigned to user")
+            )
+
+        profile_name = frappe.db.get_value(
+            "POS Profile",
+            {
+                "name": ["in", profiles],
+                "disabled": 0
+            },
+            "name"
+        )
+
+        if not profile_name:
+            frappe.throw(
+                _("No enabled POS Profile assigned to user")
+            )
+
+        require_read(
+            "POS Profile",
+            profile_name
+        )
+
+        profile = frappe.get_cached_doc(
+            "POS Profile",
+            profile_name
+        )
+
+    if profile.disabled:
+        frappe.throw(
+            _("POS Profile {0} is disabled")
+            .format(profile.name)
+        )
+
+    return profile
 
 def get_profile_defaults(pos_profile=None):
 	profile = resolve_pos_profile(pos_profile)
@@ -41,13 +81,35 @@ def get_profile_defaults(pos_profile=None):
 
 
 def get_bootstrap_data(pos_profile=None):
+
 	profile = resolve_pos_profile(pos_profile)
 	invoice_mode = get_invoice_mode()
 	data = profile_to_dict(profile, invoice_mode)
+	opening_entry = get_opening_entry(
+        frappe.session.user,
+        profile.name
+    )
 	data.update(
 		{
 			"current_user": frappe.session.user,
 			"pos_profile": profile.name,
+			 "session": {
+			"has_opening_entry": bool(opening_entry),
+			"opening_entry": opening_entry,
+			"ready": bool(opening_entry),
+ }
 		}
 	)
 	return data
+
+def get_opening_entry(user, pos_profile):
+    return frappe.db.get_value(
+        "POS Opening Entry",
+        {
+            "user": user,
+            "pos_profile": pos_profile,
+            "docstatus": 1,
+            "status": "Open"
+        },
+        "name"
+    )
